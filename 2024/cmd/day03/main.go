a2ad34a1028a0c38577088fd001cef397ca142fe
Jannik Schmeier <j.schmeier@fraport.de>
2024-12-03 22:50:40 +0100

    Day 03 Part 2

--- 2024/cmd/day03/main.go
+++ 2024/cmd/day03/main.go
@@ -14,20 +14,36 @@ func main() {
 }
 
 func solve(input string) int {
-	regex, _ := regexp.Compile("mul\\(\\d+,\\d+\\)")
+	regex, _ := regexp.Compile("mul\\(\\d+,\\d+\\)|do\\(\\)|don't\\(\\)")
+
+	//At the beginning of the program, mul instructions are enabled.
+	enabled := true
 
 	found := regex.FindAllString(input, -1)
 
 	var sum int
 	for _, value := range found {
-		numsRaw := value[4 : len(value)-1]
+		if strings.HasPrefix(value, "mul") {
+			// skip mul if not enabled
+			if !enabled {
+				continue
+			}
 
-		nums := strings.Split(numsRaw, ",")
+			numsRaw := value[4 : len(value)-1]
 
-		a := utils.Atoi(nums[0])
-		b := utils.Atoi(nums[1])
+			nums := strings.Split(numsRaw, ",")
 
-		sum += a * b
+			a := utils.Atoi(nums[0])
+			b := utils.Atoi(nums[1])
+
+			sum += a * b
+		} else if value == "do()" {
+			enabled = true
+		} else if value == "don't()" {
+			enabled = false
+		} else {
+			panic("WAT?")
+		}
 	}
 
 	return sum
